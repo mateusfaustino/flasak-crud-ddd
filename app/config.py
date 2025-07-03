6dc000c6443d8e36a4e82b944f46fdef5c87a796
mateus <mateusfaustino2024@gmail.com>
2025-07-03 20:21:16 -0300

    change db port

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ class Config:
     DB_USER = os.getenv("DB_USER", "user")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
     DB_HOST = os.getenv("DB_HOST", "localhost")
-    DB_PORT = os.getenv("DB_PORT", "3306")
+    DB_PORT = os.getenv("DB_PORT", "3307")
     DB_NAME = os.getenv("DB_NAME", "appdb")
 
     SQLALCHEMY_DATABASE_URI = os.getenv(
